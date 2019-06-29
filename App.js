c774f9d2a7fef1ce9970700e8fe4a31e62b017c4
“CGWTQe <375091805@qq.com>
2019-06-29 23:12:08 +0800

    RN_Movie初始化

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ok</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
