4870e60fb8dd1175bb9d3bc8de0266f99cd83917
“CGWTQe <375091805@qq.com>
2019-06-30 22:18:41 +0800

    2019-06-30 添加拍照

--- android/app/src/main/java/com/rn_movie/MainApplication.java
+++ android/app/src/main/java/com/rn_movie/MainApplication.java
@@ -5,6 +5,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage()
           , new VectorIconsPackage()
       );
     }
--- component/tabbars/Me.js
+++ component/tabbars/Me.js
@@ -1,20 +1,48 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+
+import {View, Button, Image} from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+var photoOptions = {
+  //底部弹出框选项
+  title: '请选择',
+  cancelButtonTitle: '取消',
+  takePhotoButtonTitle: '拍照',
+  chooseFromLibraryButtonTitle: '选择相册',
+  quality: 0.75,
+  allowsEditing: true,
+  noData: false,
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+}
 
 export default class Me extends React.Component{
-    constructor(props){
-        super(props)
-
-        this.state = {
-
-        }
+  constructor(props) {
+      super(props);
+      this.state = {
+        imgURL: 'http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=false&word=%E5%9B%BE%E7%89%87%20%E7%BE%8E%E5%A5%B3&step_word=&hs=0&pn=0&spn=0&di=149930&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=2&ie=utf-8&oe=utf-8&in=24401&cl=2&lm=-1&st=-1&cs=3408119357%2C2467563644&os=696172477%2C4001858692&simid=0%2C0&adpicid=0&lpn=0&ln=1998&fr=ala&fmq=1402900904181_R&fm=&ic=0&s=&se=1&sme=&tab=&width=&height=&face=0&ist=&jit=&cg=girl&bdtype=0&oriquery=&objurl=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fc999a12ebdc52d6be8592404254f36e4a76266241e90d-cNSF1k_fw658&fromurl=ippr_z2C%24qAzdH3FAzdH3Fi7wkwg_z%26e3Bv54AzdH3FrtgfAzdH3F0n0mb9ad0AzdH3F&gsm=0&islist=&querylist='
+      }
     }
 
     render() {
         return (
-          <View >
-            <Text>Me</Text>
+          <View style={{ alignItems:'center' ,paddingTop:100}}>
+            <Image source={{ uri: this.state.imgURL }} style={{ width: 200, height: 200,borderRadius:100 ,margin:10}}></Image>
+            <Button title="拍照" onPress={this.cameraAction}></Button>
           </View>
         );
+    }
+
+      cameraAction = () => {
+        ImagePicker.showImagePicker(photoOptions, (response) => {
+          console.log('response' + response);
+          if (response.didCancel) {
+            return
+          }
+          this.setState({
+            imgURL: response.uri
+          });
+        })
       }
 }
